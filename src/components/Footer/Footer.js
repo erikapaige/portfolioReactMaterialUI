4e7054f8bb216a250fdb2c9adc83c5a37127162c
Erika <erika.paige.handley@gmail.com>
2020-07-13 17:45:49 -0700

    delted unused material-ui elements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,14 @@
 import React from 'react'
 //material-ui elements 
 import { makeStyles } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
-import Link from '@material-ui/core/Link'
-import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import InstagramIcon from '@material-ui/icons/Instagram';
 
+// formatting for footer
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -32,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+// functions to manage onclick of icons
 const handleLinkedInChange = (event) => {
   window.location.href ='https://www.linkedin.com/in/erikapaige/'
 }
@@ -44,6 +43,7 @@ const handleInstaChange = (event) => {
   window.location.href = 'https://www.instagram.com/erikapaigestyle/'
 }
 
+// footer
 const Footer = () => {
   const classes = useStyles()
 
