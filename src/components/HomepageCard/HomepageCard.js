775c5a792649030cd7be299ffd34e9879730c6ba
Erika <erika.paige.handley@gmail.com>
2020-07-24 18:24:42 -0700

    updated skills

--- src/components/HomepageCard/HomepageCard.js
+++ src/components/HomepageCard/HomepageCard.js
@@ -58,14 +58,14 @@ const HomepageCard = () => {
             </Typography>
             <br />
             <Typography variant='body2' component='p'>
-                Freelance fashion creative looking to implement recently acquired skills in full stack web development to communicate a brand’s story. A 2020 graduate from University of California Irvine's (UCI) 12-week full stack web development bootcamp with technical skills in modern web technologies. Motivated by a challenge I know how to work collaboratively to solve problems to produce quality results that are delivered on time.
+                Accomplished fashion creative with extensive work experience, looking to implement recently acquired skills in full stack web development to communicate a brand’s story. A 2020 graduate from University of California Irvine's (UCI) 12-week full stack web development bootcamp with technical skills in modern web technologies. I know how to work collaboratively to solve problems to produce quality results that are delivered on time.
             </Typography>
             <br />
             <Typography variant='body2' component='p'>
               Skilled in the following technologies: 
               <br>
               </br>
-              HTML, CSS, Javascript, React, MySql, and Mongo.
+              Node.JS, Express, JavaScript, React.js, Bootstrap & Reactstrap, Material-UI, GitHub, GitLab, MongoDB, Sql DB, APIs, JSON, AJAX, REST syntax, The Command Line.
             </Typography>
           </CardContent>
         </Card>
