9830220ed74b3c87441dda98bfcb03550b3860b3
Erika <erika.paige.handley@gmail.com>
2020-09-04 10:10:09 -0700

    Updates

---
+++ precache-manifest.8c6a9928550602ed2c1a6cefd35be279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480c37549a150fa8b38567748b4d5b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dadd3587d7c4faa71f9",
+    "url": "/static/css/main.c0875b29.chunk.css"
+  },
+  {
+    "revision": "41961cfdccbd58c2a164",
+    "url": "/static/js/2.9808a6f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9808a6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dadd3587d7c4faa71f9",
+    "url": "/static/js/main.55f5a055.chunk.js"
+  },
+  {
+    "revision": "d145d73f02162abcd919",
+    "url": "/static/js/runtime-main.e3a4290a.js"
+  }
+]);
